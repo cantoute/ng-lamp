26edee2e39bbdb01ba0c3042121eece053370fc7
Antony GIBBS <antony@cantoute.com>
2021-08-14 07:41:17 -0400

    wip on mysql backup script

--- root-fs/root/bin/backup-mysql.sh
+++ root-fs/root/bin/backup-mysql.sh
@@ -1,56 +1,264 @@
 #!/bin/bash
 
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+verbose=0
+
+# More safety, by turning some bugs into errors.
+# Without `errexit` you don’t need ! and can replace
+# PIPESTATUS with a simple $?, but I don’t do that.
+set -o errexit -o pipefail -o noclobber -o nounset
+# set -o pipefail -o noclobber -o nounset
+
 # stop on error - if backup fails old ones aren't deleted
-set -e
-
+# set -e
 umask 027
-
 LANG="en_US.UTF-8"
 
 MYSQLDUMP=$(which mysqldump)
-BZIP='bzip2 -z'
+TIME="$(which time) --portability"
+NICE=$(which nice)
+BZ2="$(which bzip2) -z"
+GZIP="$(which gzip) -c"
+FIND="$(which find)"
+
+DRYRUN=
+
+COMPRESS="cat -"
 
 dateStamp=$(date +%F_%H%M)
 
-backupSourceHostname=$(hostname -s)
+srcHost=$(hostname -s)
 
-backupDestinationDir="/home/backups/mysql-${backupSourceHostname}"
-backupDestinationDirSingle=${backupDestinationDir}/single
+outPath="/home/backups/mysql-${srcHost}"
 
-# create missing dir
-[ ! -d "${backupDestinationDir}" ] \
-  && ( mkdir -p "${backupDestinationDir}"; chmod 700 "${backupDestinationDir}" )
+fullDumpArgs="-A --events --single-transaction"
+singleDumpArgs="--skip-lock-tables --single-transaction"
 
-[ ! -d "${backupDestinationDirSingle}" ] \
-  && ( mkdir -p "${backupDestinationDirSingle}"; chmod 700 "${backupDestinationDirSingle}" )
+# -allow a command to fail with !’s side effect on errexit
+# -use return value from ${PIPESTATUS[0]}, because ! hosed $?
+! getopt --test > /dev/null 
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
 
-if [ "$1" = "single" ]
+OPTIONS=sdto:vngbc
+LONGOPTS=single,dry-run,time,out-path:,verbose,nice,gzip,bz2,create-dirs
+
+# -regarding ! and PIPESTATUS see above
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+dryrun() {
+    if [[ ! -t 0 ]]
+    then
+        cat
+    fi
+    printf -v cmd_str '%q ' "$@"; echo "DRYRUN: Not executing $cmd_str" >&2
+}
+
+checkDirs() {
+  local args=("$@")
+
+  for d in "${args[@]}"
+    do
+      # if not is a dir
+      [[ ! -d "${d}" ]] \
+        && (
+          $DRYRUN mkdir -p "${d}";
+          $DRYRUN chmod 700 "${d}";
+        );
+    done;
+  
+  return 0;
+}
+
+single=false time=false dryRun=false verbose=false nice=false gzip=false bz2=false createDirs=false
+# now enjoy the options in order and nicely split until we see --
+
+while true; do
+  case "$1" in
+    -c|--create-dirs)
+      createDirs=true
+      shift
+      ;;
+    -s|--single)
+      single=true
+      shift
+      ;;
+    -t|--time)
+      time=true
+      shift
+      ;;
+    -d|--dry-run)
+      dryRun=true
+      shift
+      ;;
+    -v|--verbose)
+      verbose=true
+      shift
+      ;;
+    -n|--nice)
+      nice=true
+      shift
+      ;;
+    -o|--out-path)
+      outPath="$2"
+      shift 2
+      ;;
+    -g|--gzip)
+      gzip=true
+      shift
+      ;;
+    -b|--bz2)
+      bz2=true
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Programming error"
+      exit 3
+      ;;
+  esac
+done
+
+# handle non-option arguments
+# if [[ $# -ne 1 ]]; then
+#     echo "$0: A single input file is required."
+#     exit 4
+# fi
+
+[[ $dryRun = true ]] && {
+  DRYRUN="dryrun"
+}
+
+[[ $single = true ]] \
+  && outPath="${outPath}/single" \
+  || outPath="${outPath}/full"
+
+
+if [[ $createDirs = true ]]; then
+  dirs2check=("${outPath}")
+  checkDirs "${dirs2check[@]}"
+
+  # checkDirs "${outPath}"
+fi
+
+[[ $time = true ]] && {
+  MYSQLDUMP="$TIME $MYSQLDUMP"
+  FIND="$TIME $FIND"
+}
+
+
+if [[ $gzip = true ]]; then
+  COMPRESS=$GZIP
+else
+  if [[ $bz2 = true ]]; then
+    COMPRESS=$BZ2
+  fi
+fi
+
+[[ $nice = true ]] && COMPRESS="$NICE $COMPRESS"
+
+
+[[ $verbose = true ]] && \
+  echo "verbose: $verbose, dryRun: $dryRun, single: $single, outPath: $outPath, createDirs: $createDirs, nice: $nice, gzip: $gzip, bz2: $bz2"
+
+
+runBackup() {
+  local mysqldumpArgs=$fullDumpArgs
+  local db=
+
+  [[ $1 = '--db' ]] \
+    && {
+      db=$2
+      mysqldumpArgs="$singleDumpArgs $db"
+      shift
+      shift
+    }
+
+  local outFile=$1
+  local COMPRESS=
+
+  if [[ $gzip = true ]]; then
+    outFile="${outFile}.gz"
+    COMPRESS="$GZIP"
+  else
+    if [[ $bz2 = true ]]; then
+      outFile="${outFile}.bz2"
+      COMPRESS="$BZ2"
+    fi
+  fi
+
+  echo "Backing up into ${outFile}"
+
+  if [[ $gzip = true || $bz2 = true ]]; then
+    [[ $nice = true ]] && COMPRESS="$NICE $COMPRESS"
+
+    [[ $dryRun = true ]] \
+      && echo "DRYRUN: Not executing $MYSQLDUMP $mysqldumpArgs | $COMPRESS > ${outFile}" \
+      || {
+        $MYSQLDUMP $mysqldumpArgs | $COMPRESS > "${outFile}"
+      }
+  else
+    [[ $dryRun = true ]] \
+      && echo "DRYRUN: Not executing $MYSQLDUMP $mysqldumpArgs > ${outFile}" \
+      || {
+        $MYSQLDUMP $mysqldumpArgs > "${outFile}"
+      }
+  fi
+}
+
+if [[ $single = true ]];
 then
   echo "Doing single database backups..."
 
-  dbList=$(mysql -Br --silent <<< "SHOW databases WHERE \`Database\` NOT IN ('information_schema', 'performance_schema', 'mysql', 'article3_joomla', 'roundcubemail', 'www-stats_prod', 'antony', 'article3_drupal', 'postfixadmin', 'phpmyadmin') AND \`Database\` NOT LIKE 'trash\_%';")
+  dbList=$(mysql -Br --silent <<< "SHOW databases WHERE \`Database\` NOT IN ('information_schema', 'performance_schema', 'mysql') AND \`Database\` NOT LIKE 'trash\_%';")
 
   for db in ${dbList}
   do
-    printf "Processing '${db}'... "
+    echo
+    echo "Processing '${db}'... "
 
-    time $MYSQLDUMP --skip-lock-tables --single-transaction "${db}" | $BZIP > "${backupDestinationDirSingle}/mysqldump_${backupSourceHostname}_${db}_$(date +%F_%H%M).sql.bz2"
+    outFile="${outPath}/mysqldump_${srcHost}_${db}_$(date +%F_%H%M).sql"
 
-    echo "Done."
+    runBackup --db "${db}" "${outFile}"
+
+    echo Done
   done;
 
-  echo "Single database backups done."
+  echo "Single database backups done"
 else
   echo "Doing full dump (all databases)"
-  time $MYSQLDUMP -A --events --single-transaction | $BZIP > ${backupDestinationDir}/mysqldumpall_${backupSourceHostname}_${dateStamp}.sql.bz2
-  echo "Done."
+
+  outFile="${outPath}/mysqldumpall_${srcHost}_${dateStamp}.sql"
+
+  runBackup ${outFile}
+
+  echo "Done" 
 fi
 
 echo "Deleting old backups"
-# delete full backups older then 30 days
-find "${backupDestinationDir}" -type f -name 'mysqldumpall_*.sql.*' -mtime +31 -exec rm {} \;
-
-# delete single database backup older than 3 days
-find "${backupDestinationDirSingle}" -type f -name 'mysqldump_*.sql.*' -mtime +3 -exec rm {} \;
-
+if [[ $single = true ]]; then
+    # delete single database backup older than 3 days
+    $DRYRUN $FIND "${outPath}" -type f -name 'mysqldump_*.sql*' -mtime +3 -exec rm {} \;
+else
+    # delete full backups older then 30 days
+    $DRYRUN $FIND "${outPath}" -type f -name 'mysqldumpall_*.sql*' -mtime +31 -exec rm {} \;
+fi
 echo "Done"
