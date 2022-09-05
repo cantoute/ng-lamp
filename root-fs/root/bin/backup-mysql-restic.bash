1b39cad712f8f44f717ff8a6b925bc604cead02a
Antony GIBBS <antony@cantoute.com>
2022-09-05 13:53:15 -0400

    first alpha backup of mysql

--- root-fs/root/bin/backup-mysql-restic.bash
+++ root-fs/root/bin/backup-mysql-restic.bash
@@ -7,13 +7,14 @@ set -eE
 set -o errexit
 set -o pipefail
 
+RESTIC_PASSWORD=toto
 
 # set -o nounset
 
 # don't override files
 set  -o noclobber
 
-set -o xtrace
+# set -o xtrace
 
 GLOBIGNORE=*:?
 umask 027
@@ -28,8 +29,8 @@ TIME="$(which time) --portability"
 NICE=$(which nice)
 BZ2="$(which bzip2)"
 GZIP="$(which gzip)"
-# RESTIC="$(which restic)"
-RESTIC="echo restic"
+RESTIC="$(which restic)"
+# RESTIC="echo restic"
 
 # TMPDIR="$TMPDIR"
 
@@ -49,14 +50,16 @@ fileExt=".sql"
 
 compression="none"
 dumpType="full"
-bucket="."
+resticRepo="/tmp/toto"
+resticPath=""
+
+export RESTIC_PASSWORD="toto"
 
 nice=""
 
 trapSig() {
   echo "signal"
 }
-
 
 trapError() {
   local status=$?
@@ -99,8 +102,12 @@ do
       dumpType="single"
       shift
       ;;
-    -t|--target)
-      target="$2"
+    -r|--repo)
+      resticRepo="$2"
+      shift 2
+      ;;
+    -p|--path)
+      resticPath="$2"
       shift 2
       ;;
     -v|--verbose)
@@ -130,7 +137,6 @@ done
 # Ex: compress gzip mysqldump
 compress() {
   local cmp="$1"
-  # cmp="none"
 
   case "$cmp" in
     gzip|--gzip)
@@ -141,35 +147,45 @@ compress() {
       shift;
       $nice $BZ2 -z
       ;;
-    --|none|--none)
+    none|--none|--)
       shift;
-      ;;
-    "")
+      cat -
       ;;
     *)
-      shift;
-      # echo "Warning: unhandled compress argument '$1'. Not compressing."
-      # cat -
+      cat -
       ;;
+    # *)
+    #   # shift;
+    #   # echo "Warning: unhandled compress argument '$1'. Not compressing."
+    #   # cat -
+    #   ;;
   esac
+  
+  # echo "is this executed"
   # cat -
 }
 
 storeRestic() {
-  local endPoint="$1"
-  shift;
+  local repo="$1"
+  local path="$2"
+  local fileName="$3"
+  local fileExt="$4"
+  shift 4;
 
-  echo "saving to restic ${endPoint}"
+  echo "Uploading to repo:${repo} path:${path}/${fileName}${fileExt}"
   
   (
-    cat - > /dev/null
+    # cat - > /dev/null
+    $nice $RESTIC -r "${repo}" backup --stdin --stdin-filename "${path}/${fileName}${fileExt}"
   ) || {
     local error=$?
     echo "Error: Failed to send to restic"
-    exit $error
+    return $error
+  } && {
+    echo "restic success"
   }
   
-  # "$@" > "$endPoint"
+  # "$@" > "$fileName"
   
   # | $nice $RESTIC backup --stdin --stdin-filename "$1"
 
@@ -177,12 +193,57 @@ storeRestic() {
 }
 
 backup() {
-  local endPoint="$1"
-  shift;
+  local repo="$1"
+  local path="$2"
+  local fileName="$3"
+  local fileExt="$4"
+  local tmp="$5"
+  shift 5;
   
+      # local tmp=$(mktemp --tmpdir="${tmpdir}" "mysqldump-${hostName}-${timestamp}${fileExt}.XXXXXX")
+      # tmpFiles+=( "${tmp}" )
+
   # Sending to restic the compressed output of dump
-  storeRestic "$endPoint"  "$@" 
+  
     # compress $compression "$@"
+
+  # cat - >> "$tmp";
+  storeRestic "$repo" "$path" "$fileName" "$fileExt" || { 
+    local status=$?
+    echo status $status
+    echo "Storing to restic failed ${status}"
+    
+    return $status
+  } && {
+    echo "Sent to restic"
+  }
+
+  # {
+  #   cat "$tmp" | storeRestic "$fileName" || {
+  #     echo "failed %% ${tmp}"
+  #   }
+  # }
+}
+
+viaTmp() {
+  local tmp="$1"
+  shift;
+
+  # tee -a "$tmp" || {
+  #   echo "Error: not deleting tmp file ${tmp}"
+  # } && {
+  #   wait
+  #   rm -f "$tmp"
+  # }
+
+  cat - >> "$tmp" && {
+      cat "$tmp" || {
+      echo "failed %% ${tmp}"
+      return $status
+    }
+  }
+
+  
 }
 
 tmpWrap() {
@@ -230,13 +291,24 @@ tmpWrap() {
 main() {
   case "$dumpType" in
     full)
-      fileName="${hostName}-full"
+      local fileName="${hostName}-full"
+      local path
+      
+      [ "$resticPath" -eq "" ] || {
+        path="$resticPath"
+      } && {
+        path="${hostName}/mysql/full"
+      }
       
       cmd="$nice $MYSQLDUMP $dumpArgs $fullDumpArgs"
       echo $cmd;
 
-      # local tmp=$(mktemp --tmpdir="${tmpdir}" "mysqldump-${hostName}-${timestamp}${fileExt}.XXXXXX")
-      # tmpFiles+=( "${tmp}" )
+      local tmp=$(mktemp --tmpdir="${tmpdir}" "mysqldump-${hostName}-${timestamp}${fileExt}.XXXXXX")
+      tmpFiles+=( "${tmp}" )
+
+      echo "temp file ${tmp}"
+
+      
 
       # local compress
       # [$compression -eq 'none '] || {
@@ -247,7 +319,7 @@ main() {
 
       $nice $MYSQLDUMP $args \
         | compress "${compression}" \
-        | backup "${bucket}/${fileName}${fileExt}"
+        | backup "$resticRepo" "$path" "${fileName}" "${fileExt}" "$tmp"
 
       # (
       #   backup "${bucket}/${fileName}${fileExt}" \
