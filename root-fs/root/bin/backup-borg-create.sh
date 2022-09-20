69eb37d356aa4eb2a303322dcdbfe30fac8e7189
Antony GIBBS <antony@cantoute.com>
2022-09-19 22:25:24 -0400

    array of args

--- root-fs/root/bin/backup-borg-create.sh
+++ root-fs/root/bin/backup-borg-create.sh
@@ -2,43 +2,88 @@
 
 set -u
 
-backupName="$1"
+#export BORG_REPO=
+#export BORG_PASSPHRASE=
+
+backupLabel="$1"
 shift
 
 backupArgs="$@"
 
-# so we can override default repo
-# [[ "$BORG_REPO" == "" ]] && {
-#   source ~/.env.borg
-# }
-
-[[ -v BORG_REPO ]] && {
+[[ -v 'BORG_REPO' ]] && {
   echo "BORG_REPO: ${BORG_REPO}"
 }
 
-# Setting this, so the repo does not need to be given on the commandline:
-#export BORG_REPO=
+backupPrefix="{hostname}-${backupLabel}"
 
-# See the section "Passphrase notes" for more infos.
-#export BORG_PASSPHRASE=
+createArgs=(
+  --verbose
+  --list
+  --stats
+  --show-rc
+  --filter AME
+  # --compression auto,zstd,11
+  # --upload-ratelimit 30720
+)
 
-prefix="{hostname}-${backupName}"
+excludeArgs=(
+  --one-file-system
+  --exclude-caches
+  --exclude '**/lost+found'
+  --exclude '**/*nobackup*'
+  --exclude '**/.*cache*'
+  --exclude '**/.*Cache*'
+  --exclude '**/*.tmp'
+  --exclude '**/*.log'
+  --exclude '**/*.LOG'
+  --exclude '**/.npm/_*'
+  --exclude '**/.drush'
+  --exclude '**/drush-backups'
+  --exclude '**/sessions/sess_*'
+  --exclude '**/.wp-cli'
+  --exclude '**/wp-content/*cache*'
+  --exclude '**/wp-content/*log*'
+  --exclude '**/wp-content/*webp*'
+  --exclude '**/wp-content/*backup*'
+  --exclude '**/smarty/compile'
+  --exclude 'tmp'
+  --exclude 'var/tmp'
+  --exclude 'root/tmp'
+  --exclude 'home*/*/tmp'
+  --exclude 'home*/*/downloads'
+  --exclude 'home*/*/Downloads'
+  --exclude 'var/log'
+  --exclude 'var/run'
+  --exclude 'var/cache'
+  --exclude '**/var/cache'
+  --exclude '**/tmp/cache'
+  --exclude '**/site/cache'
+  --exclude 'var/lib/ntp'
+  --exclude 'var/lib/**/sessions/*'
+  --exclude 'var/lib/mysql'
+  --exclude 'var/lib/postgresql'
+  --exclude 'var/lib/postfix/*cache*'
+  --exclude 'var/lib/fail2ban/fail2ban.sqlite3'
+  --exclude 'var/lib/varnish'
+  --exclude 'var/spool/squid'
+  --exclude '**/.config/borg/security'
+  --exclude '**/.ipfs/data'
+)
 
-createArgs=
+pruneArgs=(
+  --list
+  --show-rc
+)
 
-createArgs+=" --verbose"
-createArgs+=" --list"
-createArgs+=" --stats"
-createArgs+=" --show-rc"
-
-createArgs+=" --filter AME"
-#createArgs+=" --compression lz4"
-createArgs+=" --compression auto,zstd,11"
-
-createArgs+=" --upload-ratelimit 30720" # in kiByte/s (30720 = 30Mo/s)
-createArgs+=" --upload-buffer 50" # in Mo
-
-#createArgs+=" "
+pruneKeepArgs=(
+  --keep-within   3d
+  --keep-last     10
+  --keep-hourly   12
+  --keep-daily    12
+  --keep-weekly   12
+  --keep-monthly  12
+  --keep-yearly    2
+)
 
 # some helpers and error handling:
 info() { printf "\n%s %s\n\n" "$( date )" "$*" ; }
@@ -50,51 +95,8 @@ info "Starting backup"
 # the machine this script is currently running on:
 
 borg create 				                      \
-	::"${prefix}-{now}"                     \
-	${backupArgs}                           \
-	${createArgs}                           \
-  --one-file-system                       \
-  --exclude-caches                        \
-  --exclude '**/lost+found'               \
-  --exclude '**/*nobackup*'               \
-  --exclude '**/.*cache*'                 \
-  --exclude '**/.*Cache*'                 \
-  --exclude '**/*.tmp'                    \
-  --exclude '**/*.log'                    \
-  --exclude '**/*.LOG'                    \
-  --exclude '**/.npm/_*'                  \
-  --exclude '**/.drush'                   \
-  --exclude '**/drush-backups'            \
-  --exclude '**/sessions/sess_*'          \
-  --exclude '**/.wp-cli'                  \
-  --exclude '**/wp-content/*cache*'       \
-  --exclude '**/wp-content/*log*'         \
-  --exclude '**/wp-content/*webp*'        \
-  --exclude '**/wp-content/*backup*'      \
-  --exclude '**/smarty/compile'           \
-  --exclude 'tmp'                         \
-  --exclude 'var/tmp'                     \
-  --exclude 'root/tmp'                    \
-  --exclude 'home*/*/tmp'                 \
-  --exclude 'home*/*/downloads'           \
-  --exclude 'home*/*/Downloads'           \
-  --exclude 'var/log'                     \
-  --exclude 'var/run'                     \
-  --exclude 'var/cache'                   \
-  --exclude '**/var/cache'                \
-  --exclude '**/tmp/cache'                \
-  --exclude '**/site/cache'               \
-  --exclude 'var/lib/ntp'                 \
-  --exclude 'var/lib/**/sessions/*'       \
-  --exclude 'var/lib/mysql'               \
-  --exclude 'var/lib/postgresql'          \
-  --exclude 'var/lib/postfix/*cache*'     \
-  --exclude 'var/lib/fail2ban/fail2ban.sqlite3' \
-  --exclude 'var/lib/varnish'             \
-  --exclude 'var/spool/squid'             \
-  --exclude '**/.config/borg/security'    \
-  --exclude '**/.ipfs/data'
-
+	::"${backupPrefix}-{now}"                     \
+	"${backupArgs[@]}" "${createArgs[@]}" "${excludeArgs[@]}"
 
 backup_exit=$?
 
@@ -105,29 +107,7 @@ info "Pruning repository"
 # limit prune's operation to this machine's archives and not apply to
 # other machines' archives also:
 
-pruneArgs=
-pruneArgs+=' --list'
-pruneArgs+=' --show-rc'
-pruneArgs+=' --keep-within   3d'
-pruneArgs+=' --keep-last     2'
-pruneArgs+=' --keep-hourly   12'
-pruneArgs+=' --keep-daily    12'
-pruneArgs+=' --keep-weekly   12'
-pruneArgs+=' --keep-monthly  12'
-pruneArgs+=' --keep-yearly    2'
-
-borg prune --glob-archives "${prefix}-*" ${pruneArgs}
-
-# borg prune                        \
-#   --list                          \
-#   --glob-archives "${prefix}-*"   \
-#   --show-rc                       \
-#   --keep-within   3d              \
-#   --keep-last     2               \
-#   --keep-hourly   12              \
-#   --keep-daily    12              \
-#   --keep-weekly   12              \
-#   --keep-monthly  12              \
+borg prune --glob-archives "${backupPrefix}-*" "${pruneArgs[@]}" "${pruneKeepArgs[@]}"
 
 prune_exit=$?
 
@@ -152,4 +132,3 @@ else
 fi
 
 exit ${global_exit}
-
