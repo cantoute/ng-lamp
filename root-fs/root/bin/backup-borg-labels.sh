f659b4da80b8941775f731f632a359ba318e0417
Antony GIBBS <antony@cantoute.com>
2022-09-26 19:15:21 -0400

    prettier

--- root-fs/root/bin/backup-borg-labels.sh
+++ root-fs/root/bin/backup-borg-labels.sh
@@ -6,76 +6,57 @@
 
 
 bb_label_home() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "home" /home --exclude "home/vmail" --exclude "$backupMysqlLocalDir" "$@"
 }
 
 bb_label_home_no-exclude() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "home" /home "$@"
 }
 
 bb_label_home_vmail() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "vmail" /home/vmail "$@"
 }
 
 bb_label_sys() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "sys" /etc /usr/local /root "$@"
 }
 
 bb_label_etc() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "sys" /etc "$@"
 }
 
 bb_label_usr-local() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "sys" /usr/local "$@"
 }
 
 bb_label_var() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   borgCreate "var" /var --exclude 'var/www/vhosts' "$@"
 }
 
 bb_label_mysql() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   local args=()
 
   case "${bbArg}" in
-    all|full|'')
-      ;;
+    all|full|'') ;;
 
-    single)
-      # mysql:single
-      args+=( single "${backupMysqlSingleArgs[@]}" )
-      ;;
+    single) # mysql:single
+      args+=( single "${backupMysqlSingleArgs[@]}" ) ;;
     
     *)
       info "Error: unknown argument: '$self:$bbArg'"
@@ -98,9 +79,7 @@ bb_label_sleep() {
 }
 
 bb_label_test() {
-  local self="$1"
-  local bbArg="$2"
-  shift 2
+  local self="$1" bbArg="$2"; shift 2
 
   [[ "$bbArg" == "ok" ]] || { return 128; }
 }
